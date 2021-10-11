334255f41ec396e1956190e68ffd89a5aff26b42
sarath1398 <sarath.u1398@gmail.com>
2021-10-11 18:00:50 +0530

    Initial Commit

---
+++ Audit Checklist Microservice/Controllers/AuditChecklistController.cs
@@ -0,0 +1,49 @@
+﻿using Audit_Checklist_Microservice.Service_Layer;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Audit_Checklist_Microservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuditChecklistController : ControllerBase
+    {
+
+        //public string AuditType = "Internal";
+
+        // GET: api/<AuditChecklistController>
+        [HttpGet]
+        public ActionResult<List<string>> Get(string auditType)
+        {
+
+            if (ModelState.IsValid)
+            {
+                GetQuestionsList getQuestion = new GetQuestionsList();
+
+                List<string> QuestionList = getQuestion.Questions(auditType);
+
+                if (QuestionList != null)
+                {
+                    return Ok(QuestionList);
+                }
+                else
+                {
+                    return new BadRequestObjectResult("Audit Type Mismatch!");
+                }
+            }
+
+            else
+            {
+                return new BadRequestResult();
+            }
+
+        }
+
+        
+    }
+}
---
+++ Audit Checklist Microservice/Service Layer/GetQuestionsList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Audit_Checklist_Microservice.Service_Layer
+{
+    public class GetQuestionsList
+    {
+        public List<string> Questions(string AuditType)
+        {
+            if (AuditType == "Internal")
+            {
+                return new List<string>()
+                {
+                    "Have all Change requests followed SDLC before PROD move?",
+                    "Have all Change requests been approved by the application owner?",
+                    "Are all artifacts like CR document, Unit test cases available?",
+                    "Is the SIT and UAT sign-off available?",
+                    "Is data deletion from the system done with application owner approval?"
+                };
+            }
+            else if (AuditType == "SOX")
+            {
+                return new List<string>()
+                {
+                    "Have all Change requests followed SDLC before PROD move?",
+                    "Have all Change requests been approved by the application owner?",
+                    "For a major change, was there a database backup taken before and after PROD move?",
+                    "Has the application owner approval obtained while adding a user to the system?",
+                    "Is data deletion from the system done with application owner approval?"
+                };
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
